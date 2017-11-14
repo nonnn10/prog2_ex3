775513d96a0ef3bb43441afbf70e653ad36bd494
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2017-11-14 11:46:07 +0900

    first commit

---
+++ src/Dice.java
@@ -0,0 +1,17 @@
+package jp.ac.uryukyu.ie.ex3.pair15;                                            
+                                                                                
+public class Dice{                                                          
+    private int value;                                                      
+    public Dice(){                                                          
+        play();                                                                 
+    }                                                                           
+    public int getValue(){                                                      
+        return value;                                                           
+    }                                                                           
+    public void setValue(int value){                                            
+        this.value = value;                                                     
+    }                                                                           
+    public void play(){                                                         
+        value = (int)(Math.random()*6) + 1;                                     
+    }                                                                           
+}     
