85be0468c5d53e331b0c384243541fbc7d0360b1
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2017-12-05 11:39:32 +0900

    １回目Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-import jp.ac.uryukyu.ie.ex3.pair15.Dice;                                        
-                                                                                
-public class Main{                                                              
-    public static void main (String[] args){                                    
-	Dice dice = new Dice();                                                     
-	dice.play();                                                                
-	System.out.println(dice.getValue());                                        
-    }                                                                           
-} 
+
+import jp.ac.uryukyu.ie.ex3.pair23.Dice;
+
+public class Main{
+    public static void main (String[] args){
+    Dice dice = new Dice();
+    
+dice.play();
+    System.out.println(dice.getValue());
+    }
+}
